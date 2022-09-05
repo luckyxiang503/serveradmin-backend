9a86926e5e7d4879b3bfc5f130f4f1b724cafdc9
zhang_xiang <zhang_xiang@haici.com>
2022-09-05 17:44:57 +0800

    用户登录接口完善

--- api/login.py
+++ api/login.py
@@ -12,12 +12,12 @@ login = APIRouter(tags=["认证相关"])
 async def user_login(form_data: OAuth2PasswordRequestForm = Depends()):
     db_user = get_user_by_name(username=form_data.username)
     if db_user is None:
-        raise HTTPException(status_code=400, detail="用户名错误!")
+        raise HTTPException(status_code=404, detail="用户不存在")
     if verify_password(form_data.password, db_user.password):
         token = create_access_token(data={"sub": db_user.username})
-        return ResponseToken(access_token=token)
+        return ResponseToken(data={"token": f"bearer {token}"}, access_token=token)
     else:
-        raise HTTPException(status_code=400, detail="密码错误!")
+        raise HTTPException(status_code=400, detail="密码错误")
 
 
 @login.put("/logout", summary="注销")
--- api/user.py
+++ api/user.py
@@ -3,7 +3,7 @@ from fastapi import HTTPException, APIRouter, Depends
 
 from db.crud import get_user_by_name, create_user, get_users
 from schemas.user import User
-from schemas.base import Response200, Response400
+from schemas.base import Response200
 from core.security import get_hash_password, get_current_user
 
 
@@ -11,11 +11,8 @@ user = APIRouter(tags=['用户相关'], dependencies=[Depends(get_current_user)]
 
 
 @user.get("/user", summary='获取用户信息')
-async def get_user_info(username: str):
-    db_user = get_user_by_name(username=username)
-    if db_user is None:
-        raise HTTPException(status_code=404, detail="用户不存在")
-    return Response200(data=db_user, msg="用户信息查询成功")
+async def get_user_info(userinfo: User = Depends(get_current_user)):
+    return Response200(data=userinfo, msg="用户信息查询成功")
 
 
 @user.get("/users", response_model=List[User], summary='用户列表',)
