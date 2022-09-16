a42797b183f1ec2ef2d8889053a1eedfaf6cb8ad
zhang_xiang <zhang_xiang@haici.com>
2022-09-16 17:00:09 +0800

    服务安装功能完善 添加server库和serverhost库

--- api/websocket.py
+++ api/websocket.py
@@ -1,54 +1,28 @@
 from fastapi import WebSocket, APIRouter
-from fastapi.responses import HTMLResponse
+import time
 
-from core.server import read_server_log
+from config import settings
 
 ws = APIRouter(tags=["websocket"])
-
-
-html = """
-<!DOCTYPE html>
-<html>
-    <head>
-        <title>Chat</title>
-    </head>
-    <body>
-        <h1>WebSocket Chat</h1>
-        <form action="" onsubmit="sendMessage(event)">
-            <input type="text" id="messageText" autocomplete="off"/>
-            <button>Send</button>
-        </form>
-        <ul id='messages'>
-        </ul>
-        <script>
-            var ws = new WebSocket("ws://localhost:8000/api/wslog");
-            ws.onmessage = function(event) {
-                var messages = document.getElementById('messages')
-                var message = document.createElement('li')
-                var content = document.createTextNode(event.data)
-                message.appendChild(content)
-                messages.appendChild(message)
-            };
-            function sendMessage(event) {
-                var input = document.getElementById("messageText")
-                ws.send(input.value)
-                input.value = ''
-                event.preventDefault()
-            }
-        </script>
-    </body>
-</html>
-"""
-
-
-@ws.get("/wsclient")
-async def get():
-    return HTMLResponse(html)
 
 
 @ws.websocket('/wslog')
 async def send_server_log(websocket: WebSocket):
     await websocket.accept()
+    file = settings.logfile
+    f = open(file)
+
     while True:
-        data = await read_server_log()
-        await websocket.send_text(f"server: {data}")
+        where = f.tell()
+        line = f.readline().replace('\n', '')
+        if not line:
+            time.sleep(1)
+            f.seek(where)
+            data = ''
+        else:
+            data = line
+        await websocket.send_text(data)
+        r = await websocket.receive_text()
+        if r == "close":
+            await websocket.close()
+            break
